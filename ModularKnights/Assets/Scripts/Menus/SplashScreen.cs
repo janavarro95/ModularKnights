f87fac4e72b442478c23d2bd951e468f71d0d90c
Joshua Navarro <janavarro95@gmail.com>
2019-02-16 01:10:35 -0800

    Got a really nice splash screen going.

---
+++ ModularKnights/Assets/Scripts/Menus/SplashScreen.cs
@@ -0,0 +1,59 @@
+﻿using Assets.Scripts.Utilities;
+using Assets.Scripts.Utilities.Delegates;
+using Assets.Scripts.Utilities.Timers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Menus
+{
+    public class SplashScreen:Menu
+    {
+        DeltaTimer timerToShow;
+
+
+        public override void Start()
+        {
+            Menu.ActiveMenu = this;
+
+            ScreenTransitions.StartSceneTransition(3, "", Color.white, ScreenTransitions.TransitionState.FadeIn,new VoidDelegate(fadeInFinished));
+            timerToShow = new DeltaTimer(3, Enums.TimerType.CountDown, false, new VoidDelegate(fadeToMainMenu));
+
+            this.menuCursor = GameInput.GameCursor.Instance;
+        }
+
+        public override void Update()
+        {
+            timerToShow.Update();
+        }
+
+        private void fadeInFinished()
+        {
+            timerToShow.start();
+        }
+        private void fadeToMainMenu()
+        {
+            Scripts.Utilities.ScreenTransitions.StartSceneTransition(3, "MainMenu", Color.white, Utilities.ScreenTransitions.TransitionState.FadeOut);
+        }
+
+        public override void exitMenu()
+        {
+            base.exitMenu();
+        }
+
+        public override bool snapCompatible()
+        {
+            return false;
+        }
+
+        public override void setUpForSnapping()
+        {
+            //do nothing.
+        }
+
+
+    }
+}
