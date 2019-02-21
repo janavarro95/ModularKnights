b7ae3010a7c5968ccc7deb5edb40d617dc66632c
Joshua Navarro <janavarro95@gmail.com>
2019-02-20 16:47:00 -0800

    Added serialization to/from file system.

--- ModularKnights/Assets/Scripts/Utilities/Serialization/Serializer.cs
+++ ModularKnights/Assets/Scripts/Utilities/Serialization/Serializer.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using SFB;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -140,5 +141,68 @@ namespace Assets.Scripts.Utilities.Serialization
                 return JsonConvert.DeserializeObject(info, T, this.settings);
             }
 
+        /// <summary>
+        /// Seralize the given object to a json file on the file system. Fails if no valid path is selected to save to.
+        /// </summary>
+        /// <param name="o"></param>
+        public void Serialize(object o)
+        {
+            var path = StandaloneFileBrowser.SaveFilePanel("Save File", "", "", "");
+
+            if (String.IsNullOrEmpty(path))
+            {
+                return;
+            }
+            else
+            {
+                Serialize(path, o);
+            }
+
         }
+
+        /// <summary>
+        /// Deserialize the selected .json file from the file system. Fails if a file is not selected.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T Deserialize<T>()
+        {
+            var extensions = new[] {
+            new ExtensionFilter("Content File", "json"),
+            new ExtensionFilter("All Files", "*" ),
+            };
+            var paths = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, true);
+
+            if (paths.Length == 0) return default(T);
+            if (String.IsNullOrEmpty(paths[0]))
+            {
+                return default(T);
+            }
+
+            return Deserialize<T>(paths[0]);
+        }
+
+        /// <summary>
+        /// Deserialize the selected .json file from the file system.
+        /// </summary>
+        /// <param name="T"></param>
+        /// <returns></returns>
+        public object Deserialize(Type T)
+        {
+            var extensions = new[] {
+            new ExtensionFilter("Content File", "json"),
+            new ExtensionFilter("All Files", "*" ),
+            };
+            var paths = StandaloneFileBrowser.OpenFilePanel("Open File", "", extensions, true);
+
+            if (paths.Length == 0) return null;
+            if (String.IsNullOrEmpty(paths[0]))
+            {
+                return null;
+            }
+
+            return Deserialize(paths[0],T);
+        }
+
+    }
 }
